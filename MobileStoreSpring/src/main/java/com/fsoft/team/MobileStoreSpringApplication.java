1409a0b29eb58072961e6ea1acab8e23ce27aff0
caoquy2000 <caohoanghuy2000@gmail.com>
2021-06-17 08:53:54 +0700

    init

---
+++ MobileStoreSpring/src/main/java/com/fsoft/team/MobileStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fsoft.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileStoreSpringApplication.class, args);
+	}
+
+}
